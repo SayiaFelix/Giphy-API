d43565463cf300260de379bf5d327390b78dfdb4
SayiaFelix <sayia.lucas@student.moringaschool.com>
2022-04-14 19:07:09 +0300

    adding files

---
+++ src/app/gifs/gifs.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { DataService } from '../data.service';
+
+@Component({
+  selector: 'app-gifs',
+  templateUrl: './gifs.component.html',
+  styleUrls: ['./gifs.component.css']
+})
+export class GifsComponent implements OnInit, OnDestroy {
+
+
+gifs: any[] | undefined;
+  subscription: Subscription = new Subscription;
+
+
+
+  constructor(private dataservice:DataService) { }
+
+  ngOnInit(): void {
+    this.dataservice.getTrendingGifs()
+    this.subscription = this.dataservice.getGifs()
+   .subscribe((response:any)=>{
+
+    this.gifs=response;
+  })
+  }
+
+ngOnDestroy(): void {
+  
+this.subscription.unsubscribe();
+}
+
+
+
+}
